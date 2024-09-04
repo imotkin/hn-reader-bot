0124ce0096e1073328fc61b62452d587df02b847
Ilya Motkin <imotkin@outlook.com>
2024-09-04 15:38:08 +0300

    Add token flag

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"cmp"
+	"flag"
 	"log"
 	"os"
 )
 
 func main() {
-	token := os.Getenv("BOT_TOKEN")
+	tokenFlag := flag.String("token", "", "The token to connect Telegram API")
+	flag.Parse()
+
+	token := cmp.Or(*tokenFlag, os.Getenv("BOT_TOKEN"))
 	bot := NewBot(token, true)
 
 	if err := bot.Init(); err != nil {
